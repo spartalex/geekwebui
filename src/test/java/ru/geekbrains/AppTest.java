f582610a437b4b99b0ed3a14c2d69ce351f03724
a.smirnov <a.smirnov@qiwi.com>
2021-04-26 11:56:29 +0300

    initinal

---
+++ src/test/java/ru/geekbrains/AppTest.java
@@ -0,0 +1,30 @@
+package ru.geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void firstTestExampleTrue() {
+        System.out.println("First test example");
+        assertTrue(true);
+    }
+
+    @Test
+    public void secondTestExample() {
+        System.out.println("Second test example");
+        assertTrue(true);
+    }
+}
